d4ef50051183cdb4901524e96ec2df763443320d
wegert <wegert@edu.htl-villach.at>
2016-03-15 14:55:06 +0100

    split js enhanced

--- js/js.js
+++ js/js.js
@@ -2,17 +2,37 @@
 
 $(document).ready(function () {
     "use strict";
-    $("#split").on("mouseover", function () {
-        var width = $(document).width();
+    
+    $("#split").on("mousemove", function () {
+        var width = $(window).width();
         var curPosX = event.pageX;
+        
         var leftSplit = 100 / width * curPosX;
         var rightSplit = 100 - leftSplit;
         
         var left = leftSplit + "%";
         var right = rightSplit + "%";
         
-        $(this).children("#left-split").css( "width", left );
-        $(this).children("#right-split").css( "width", right );        
+        var widthLeftSplit = $("#left-split").width();
+        var widthRightSplit = $("#right-split").width();
+
+        var widthLeftTitle = $("#left-split").find(".wrapTitle").children("h1").width() + 100;
+        var widthRightTitle = $("#right-split").find(".wrapTitle").children("h1").width() + 100;
+        
+        if (widthLeftSplit > widthLeftTitle && widthRightSplit > widthRightTitle) {
+            
+            if (curPosX > widthLeftTitle && curPosX < width - widthRightTitle) {
+                $(this).children("#left-split").css( "width", left );
+                $(this).children("#right-split").css( "width", right );
+            }
+            else {
+                
+                leftPercent = widthLeftTitle + "%";
+                rightPercent = widthRightTitle + "%";
+                $(this).children("#left-split").css( "width", leftPercent );
+                $(this).children("#left-split").css( "width", rightPercent );
+            }
+        }
     });
     
     $("#split").on("mouseout", function() {
