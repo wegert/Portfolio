ae5d92dd158370fb3c1dc54a856d1857e75aa031
wegert <wegert@edu.htl-villach.at>
2016-03-15 23:00:00 +0100

    fixed bug on split

--- js/js.js
+++ js/js.js
@@ -7,6 +7,10 @@ $(document).ready(function () {
         var width = $(window).width();
         var curPosX = event.pageX;
         
+        console.log(width);
+        
+        var minVal = width / 5;
+        
         var leftSplit = 100 / width * curPosX;
         var rightSplit = 100 - leftSplit;
         
@@ -19,24 +23,30 @@ $(document).ready(function () {
         var widthLeftTitle = $("#left-split").find(".wrapTitle").children("h1").width() + 100;
         var widthRightTitle = $("#right-split").find(".wrapTitle").children("h1").width() + 100;
         
-        if (widthLeftSplit > widthLeftTitle && widthRightSplit > widthRightTitle) {
-            
-            if (curPosX > widthLeftTitle && curPosX < width - widthRightTitle) {
-                $(this).children("#left-split").css( "width", left );
-                $(this).children("#right-split").css( "width", right );
-            }
-            else {
-                
-                leftPercent = widthLeftTitle + "%";
-                rightPercent = widthRightTitle + "%";
-                $(this).children("#left-split").css( "width", leftPercent );
-                $(this).children("#left-split").css( "width", rightPercent );
-            }
+        var leftMin = 100 / width * minVal;
+        var rightMax = 100 - leftMin;
+        
+        var leftPercent = leftMin + "%";
+        var rightPercent = rightMax + "%";
+        
+        if (curPosX >= minVal && curPosX <= width - minVal ) {
+            $(this).children("#left-split").animate({width: left}, 13, 'swing');
+            $(this).children("#right-split").animate({width: right}, 13, 'swing');
+        }
+        else if (curPosX < minVal) {
+            $(this).children("#left-split").css( "width", leftPercent );
+            $(this).children("#right-split").css( "width", rightPercent );
+        }
+        else if (curPosX > width - minVal) {
+            $(this).children("#left-split").css( "width", rightPercent );
+            $(this).children("#right-split").css( "width", leftPercent );
         }
     });
     
+    /*
     $("#split").on("mouseout", function() {
         $(this).children("#left-split").css( "width", "50%" );
         $(this).children("#right-split").css( "width", "50%" );
     });
+    */
 });
